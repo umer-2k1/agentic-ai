27093653f074577abe989391212aec4f344350ec
umer-2k1 <umermemon4648@gmail.com>
2025-05-27 05:41:52 +0500

    Update llm_evaluator.py

--- recruitment/agents/llm_evaluator.py
+++ recruitment/agents/llm_evaluator.py
@@ -4,7 +4,6 @@ import json
 from langchain_core.output_parsers import JsonOutputParser
 
 
-
 class LLMEvaluator:
     def __init__(self, api_key: str):
         self.llm = ChatGroq(
@@ -12,7 +11,7 @@ class LLMEvaluator:
             model="meta-llama/llama-4-scout-17b-16e-instruct",
             temperature=0.3,
         )
-        
+
         self.output_parser = JsonOutputParser()
 
     def evaluate_candidate(self, candidate, job_desc):
@@ -41,22 +40,21 @@ class LLMEvaluator:
         }}
 
         """
-        print("prompt....", prompt)
         messages = [
             (
                 "system",
                 "You are an expert technical recruiter. Evaluate the candidate based on job requirements.",
             ),
-            ("human", prompt)
+            ("human", prompt),
         ]
         try:
             response = self.llm.invoke(messages)
             parse_result = self.output_parser.parse(response.content)
-            print("RAW RESPONSE:", parse_result)
-            # print("LLM.............", parse_result)
+
             return parse_result
         except Exception as e:
             import traceback
+
             print(traceback.print_exc())
             print(f"LLM error: {e}")
             return {
